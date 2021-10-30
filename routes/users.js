c909841dbbd8d33bf147a62919d604b99db9e9bc
Shreyas <shreyas.mm@somaiya.edu>
2021-10-30 12:45:43 +0530

    Few pages

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,12 @@ router.get('/signup', function(req, res, next) {
 router.get('/dashboard', function(req, res, next) {
   res.render('user/dashboard');
 });
+
+router.get('/projects', function(req, res, next) {
+  res.render('user/projects');
+});
+
+router.get('/projectDetails', function(req, res, next) {
+  res.render('user/projectDetails');
+});
 module.exports = router;
